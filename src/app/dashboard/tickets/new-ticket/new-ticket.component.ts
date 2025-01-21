77c008b970a4457b7dd22959de53970daed18795
Abdul_Malik <malik.info28@gmail.com>
2025-01-21 15:56:01 +0530

    using ViewChild Decorator

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -6,10 +7,17 @@ import { ControlComponent } from "../../../shared/control/control.component";
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+
+    this.form?.nativeElement.reset();
+  }
 }
