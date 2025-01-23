5d33300198b3e53e91493ec54be5c2d3e62bb263
Abdul_Malik <malik.info28@gmail.com>
2025-01-23 22:04:02 +0530

    switched to two-way binding

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,48 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
+// import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// import { ButtonComponent } from '../../../shared/button/button.component';
+// import { ControlComponent } from "../../../shared/control/control.component";
+
+// @Component({
+//   selector: 'app-new-ticket',
+//   standalone: true,
+//   imports: [ButtonComponent, ControlComponent, FormsModule],
+//   templateUrl: './new-ticket.component.html',
+//   styleUrl: './new-ticket.component.css'
+// })
+// export class NewTicketComponent implements OnInit, AfterViewInit {
+//   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+//   // private form = viewChild<ElementRef<HTMLFormElement>>('form');
+//   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+//   // @Output() add = new EventEmitter<{ title: string, text: string }>();
+//   add = output<{ title: string, text: string }>();
+
+//   constructor() {
+//     console.log('constructor');
+//     console.log(this.form?.nativeElement);
+//   }
+
+//   ngOnInit() {
+//     console.log('ON INIT nwtckt');
+//     console.log(this.form?.nativeElement);
+//   }
+
+//   ngAfterViewInit() {
+//     console.log('AFTER VIEW INIT');
+//     console.log(this.form?.nativeElement);
+//   }
+
+//   onSubmit(title: string, ticketText: string) {
+//     this.add.emit({ title: title, text: ticketText });
+
+//     // this.form()?.nativeElement.reset();
+//     this.form?.nativeElement.reset();
+//   }
+// }
+
+import { AfterViewInit, Component, ElementRef, OnInit, output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -13,10 +57,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
 })
 export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  // private form = viewChild<ElementRef<HTMLFormElement>>('form');
-  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
-
-  // @Output() add = new EventEmitter<{ title: string, text: string }>();
+  enteredTitle = '';
+  enteredText = '';
   add = output<{ title: string, text: string }>();
 
   constructor() {
@@ -34,10 +76,11 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({ title: title, text: ticketText });
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
 
-    // this.form()?.nativeElement.reset();
-    this.form?.nativeElement.reset();
+    // this.form?.nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
